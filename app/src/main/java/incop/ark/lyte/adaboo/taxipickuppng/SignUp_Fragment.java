c58ce7e5354a028b7cc1d14aa7cd7665580381c7
Arnold AZ <arnoldazeem2008@gmail.com>
2017-10-07 17:18:39 +0200

    Volley

--- app/src/main/java/incop/ark/lyte/adaboo/taxipickuppng/SignUp_Fragment.java
+++ app/src/main/java/incop/ark/lyte/adaboo/taxipickuppng/SignUp_Fragment.java
@@ -2,27 +2,50 @@ package incop.ark.lyte.adaboo.taxipickuppng;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.kosalgeek.android.md5simply.MD5;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static android.content.ContentValues.TAG;
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class SignUp_Fragment extends Fragment implements OnClickListener {
 	private static View view;
@@ -34,6 +57,12 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 	private static CheckBox terms_conditions;
 
 
+    RequestQueue requestQueue;
+    ProgressDialog progress;
+    JSONArray products  = null;
+    GridView grid;
+    String finals;
+    String urls;
     String getFullName;
     String getEmailId;
     String getMobileNumber;
@@ -42,6 +71,13 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
     String getZipcode;
     String getPassword;
     String getConfirmPassword;
+	String getencryptedpassword;
+
+    private static String S_URL ="https://vast-springs-89039.herokuapp.com/register";
+
+    CheckBox checkBoxTerms;private Snackbar snackbar;
+    //private ProgressDialog pd;
+    ProgressBar  pd;
 
 	public SignUp_Fragment() {
 
@@ -53,6 +89,7 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 		view = inflater.inflate(R.layout.register, container, false);
 		initViews();
 		setListeners();
+
 		return view;
 	}
 
@@ -110,6 +147,8 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 		 getPassword = password.getText().toString();
 		 getConfirmPassword = confirmPassword.getText().toString();
 
+		getencryptedpassword = MD5.encrypt(getPassword);
+
 		// Pattern match for email id
 		Pattern p = Pattern.compile(Utils.regEx);
 		Matcher m = p.matcher(getEmailId);
@@ -144,15 +183,148 @@ public class SignUp_Fragment extends Fragment implements OnClickListener {
 					"Please select Terms and Conditions.");
 
 		// Else do signup or do your stuff
-		else
+		else {
 			Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
 					.show();
 
-       // doStaff();
+            try {
+                signupRequest();
+            }catch (Exception E){
+               E.printStackTrace();
+            }
+
+		}
 
 	}
 
 
+
+
+    public void UserRegistration(){
+
+        // Showing progress dialog at user registration time.
+        //progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
+        //progressDialog.show();
+
+        finals = "https://vast-springs-89039.herokuapp.com/register";
+
+        // Creating string request with post method.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, finals,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+
+                        // Hiding the progress dialog after all task complete.
+                       // progressDialog.dismiss();
+
+                        // Showing Echo Response Message Coming From Server.
+                        Toast.makeText(getActivity(), ServerResponse, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        // Hiding the progress dialog after all task complete.
+                       // progressDialog.dismiss();
+
+                        // Showing error message if something goes wrong.
+                        Toast.makeText(getActivity(), volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // Adding All values to Params.
+                // The firs argument should be same sa your MySQL database table columns.
+                params.put("firstName", getMobileNumber);
+                params.put("User_Password", getPassword);
+                //params.put("User_Full_Name", NameHolder);
+
+                return params;
+            }
+
+                @Override
+                public Map<String, String> getHeaders(){
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                String creds = String.format("%s:%s","taxiUserBeta","taxiUserBetaPass123");
+                String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+                params.put("Authorization", auth);
+                return params;
+            }
+
+        };
+
+        // Creating RequestQueue.
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        // Adding the StringRequest object into requestQueue.
+        requestQueue.add(stringRequest);
+
+    }
+
+
+    private void signupRequest() throws Exception{
+
+
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        finals = "https://vast-springs-89039.herokuapp.com/register";
+
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("firstName", "Rocky");
+        jsonBody.put("lastName", "Balboa");
+
+        final String mRequestBody = jsonBody.toString();
+
+        RequestQueue mQueue = Volley.newRequestQueue(getActivity());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, finals, jsonBody, new Response.Listener<JSONObject>() {
+
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("TAG", response + "toString()");
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+
+
+        }){
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                String creds = String.format("%s:%s","taxiUserBeta","taxiUserBetaPass123");
+                String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+                params.put("Authorization", auth);
+                return params;
+            }
+
+        };
+
+
+        mQueue.add(jsonObjectRequest);
+
+    }
+
+
+
+    public void showSnackbar(String stringSnackbar){
+        //snackbar.make(findViewById(android.R.id.content), stringSnackbar.toString(), Snackbar.LENGTH_SHORT)
+          //      .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+          //      .show();
+    }
 
 
 }
